2e078b91acf1843f75ea2345144bea46510f0300
Richard White <w@rwhite.no>
2019-11-28 09:10:13 +0200

    Update data_gen_all.r

--- R/data_gen_all.r
+++ R/data_gen_all.r
@@ -62,9 +62,9 @@ gen_data_all <- function(base_loc) {
   # map labels
 
   norway_map_counties_label_positions_b2019 <- gen_norway_map_counties_label_positions(x_year_end=2020)
-  save(norway_map_counties_label_positions_b2019, file=file.path("/git","/fhidata","data","norway_map_counties_label_positions_b2019.rda"), compress = "xz")
+  save(norway_map_counties_label_positions_b2019, file=file.path(base_loc,"norway_map_counties_label_positions_b2019.rda"), compress = "xz")
   norway_map_counties_label_positions_b2020 <-  gen_norway_map_counties_label_positions(x_year_end=2020)
-  save(norway_map_counties_label_positions_b2020, file=file.path("/git","/fhidata","data","norway_map_counties_label_positions_b2020.rda"), compress = "xz")
+  save(norway_map_counties_label_positions_b2020, file=file.path(base_loc,"norway_map_counties_label_positions_b2020.rda"), compress = "xz")
 
   # norway_map_counties_b2019 <- gen_norway_map_counties(x_year_end=2019)
   # save(norway_map_counties_b2019, file=file.path("/git","/fhidata","data","norway_map_counties_b2019.rda"), compress = "xz")
